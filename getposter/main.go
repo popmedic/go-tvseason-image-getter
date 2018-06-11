499e7a01ce3302d197e779f1048a524468b2de83
kscardina <popmedic@gmail.com>
2018-06-11 09:25:10 -0600

    clean up

--- getposter/main.go
+++ getposter/main.go
@@ -20,6 +20,20 @@ import (
 	"github.com/popmedic/go-tvseason-image-getter/tmdb"
 )
 
+const (
+	exitCodeShowNotSet                       = 10
+	exitCodeUnableToGetConfig                = 20
+	exitCodeUnableToQueryShows               = 30
+	exitCodeNoMatchingShowResults            = 40
+	exitCodeUnableToGetSeason                = 50
+	exitCodeNoPoster                         = 60
+	exitCodeDownloadError                    = 70
+	exitCodeUnableToCreateOutFile            = 80
+	exitCodeUnableToDecodePosterImage        = 90
+	exitCodeUnableToEncodeResizedPosterImage = 100
+	exitCodeUnableToCopyPosterImage          = 110
+)
+
 var showName = flag.String("show", "", "the show name ** required **")
 var seasonNumber = flag.Int("season", -1, "the season number")
 var out = flag.String("out", "", "file to output, defaults to \"Season <season>-SD.jpg\", or \"<show>-SD.jpg\" if no season is given.")
@@ -50,7 +64,7 @@ func main() {
 	// Make sure that the showName flag is set
 	if len(*showName) == 0 {
 		flag.Usage()
-		log.Fatal(func(int) { os.Exit(1) }, "show must be set")
+		log.Fatal(func(int) { os.Exit(exitCodeShowNotSet) }, "show must be set")
 	}
 
 	if isShow() {
@@ -62,17 +76,17 @@ func main() {
 	cfg, err := tmdb.GetConfig(tmdb.HttpGetter(http.Get))
 	if err != nil {
 		flag.Usage()
-		log.Fatal(func(int) { os.Exit(2) }, err)
+		log.Fatal(func(int) { os.Exit(exitCodeUnableToGetConfig) }, err)
 	}
 
 	showQuery, err := tmdb.QueryShows(*showName, tmdb.HttpGetter(http.Get))
 	if err != nil {
 		flag.Usage()
-		log.Fatal(func(int) { os.Exit(3) }, err)
+		log.Fatal(func(int) { os.Exit(exitCodeUnableToQueryShows) }, err)
 	}
 
 	if len(showQuery.Results) <= 0 {
-		log.Fatalf(func(int) { os.Exit(3) }, "no show results matching %q", *showName)
+		log.Fatalf(func(int) { os.Exit(exitCodeNoMatchingShowResults) }, "no show results matching %q", *showName)
 	}
 
 	showResult := showQuery.GetClosestResult(*showName)
@@ -88,17 +102,17 @@ func main() {
 	var url string
 	if isShow() {
 		if len(showResult.PosterPath) == 0 {
-			log.Fatal(func(int) { os.Exit(5) }, fmt.Sprintf("%q does not have a poster", showResult.Name))
+			log.Fatal(func(int) { os.Exit(exitCodeNoPoster) }, fmt.Sprintf("%q does not have a poster", showResult.Name))
 		}
 		url = cfg.Images.SecureBaseUrl + "original" + showResult.PosterPath
 	} else {
 		season, err := tmdb.GetSeason(showResult.ID, *seasonNumber, tmdb.HttpGetter(http.Get))
 		if err != nil {
 			flag.Usage()
-			log.Fatal(func(int) { os.Exit(4) }, err)
+			log.Fatal(func(int) { os.Exit(exitCodeUnableToGetSeason) }, err)
 		}
 		if len(season.PosterPath) == 0 {
-			log.Fatal(func(int) { os.Exit(5) }, fmt.Sprintf("%q season %d does not have a poster", *showName, *seasonNumber))
+			log.Fatal(func(int) { os.Exit(exitCodeNoPoster) }, fmt.Sprintf("%q season %d does not have a poster", *showName, *seasonNumber))
 		}
 		url = cfg.Images.SecureBaseUrl + "original" + season.PosterPath
 	}
@@ -107,19 +121,19 @@ func main() {
 
 	data, err := download(url)
 	if err != nil {
-		log.Fatal(func(int) { os.Exit(6) }, err)
+		log.Fatal(func(int) { os.Exit(exitCodeDownloadError) }, err)
 	}
 	// Create the out file
 	outf, err := os.Create(*out)
 	if err != nil {
-		log.Fatal(func(int) { os.Exit(7) }, err)
+		log.Fatal(func(int) { os.Exit(exitCodeUnableToCreateOutFile) }, err)
 	}
 	defer outf.Close()
 	// Set the resize width and height if given
 	if *width > 0 && *height > 0 {
 		image, _, err := img.Decode(data)
 		if nil != err {
-			log.Fatal(os.Exit, err)
+			log.Fatal(func(int) { os.Exit(exitCodeUnableToDecodePosterImage) }, err)
 		}
 		newImage := resize.Resize(uint(*width), uint(*height), image, resize.Lanczos3)
 		switch strings.ToLower(filepath.Ext(*out)) {
@@ -131,12 +145,12 @@ func main() {
 			err = errors.New("unknown type \"" + strings.ToUpper(filepath.Ext(*out)) + "\"")
 		}
 		if nil != err {
-			log.Fatal(func(int) { os.Exit(8) }, err)
+			log.Fatal(func(int) { os.Exit(exitCodeUnableToEncodeResizedPosterImage) }, err)
 		}
 	} else {
 		_, err = io.Copy(outf, data)
 		if err != nil {
-			log.Fatal(func(int) { os.Exit(9) }, err)
+			log.Fatal(func(int) { os.Exit(exitCodeUnableToCopyPosterImage) }, err)
 		}
 	}
 
